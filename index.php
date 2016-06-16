3bb9697a58396d732760cfabdc1d20da66349864
unknown <e.karakulov@comp20.astkhabar.lc>
2016-06-16 20:32:49 +0600

    sending a letter

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<?php
+// пример использования
+require_once "SendMailSmtpClass.php"; // подключаем класс
+  
+$mailSMTP = new SendMailSmtpClass('ereke_enu@mail.ru', '***', 'ssl://smtp.mail.ru', 'Yerlan', 465); // создаем экземпляр класса
+// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'имя отправителя');
+  
+// заголовок письма
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+$headers .= "From: Yerlan <ereke_enu@mail.ru>\r\n"; // от кого письмо !!! тут e-mail, через который происходит авторизация
+$result =  $mailSMTP->send('almasmyltykbayev@mail.ru', 'Тема письма', 'Текст письма', $headers); // отправляем письмо
+// $result =  $mailSMTP->send('Кому письмо', 'Test', 'Текст письма', 'Заголовки письма');
+if($result === true){
+    echo "Письмо успешно отправлено";
+}else{
+    echo "Письмо не отправлено. Ошибка: " . $result;
+}
+
+
+
+?>
