6df9b4773c8859934de4e4b7f25007af727cf3d8
unknown <e.karakulov@comp20.astkhabar.lc>
2016-06-23 18:36:55 +0600

    some changes

--- index.php
+++ index.php
@@ -1,43 +1,34 @@
 ﻿<?php
 
-
-
-/*
-// пример использования
-require_once "SendMailSmtpClass.php"; // подключаем класс
-  
-$mailSMTP = new SendMailSmtpClass('ereke_enu@mail.ru', '***', 'ssl://smtp.mail.ru', 'Yerlan', 465); // создаем экземпляр класса
-// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'имя отправителя');
-  
-// заголовок письма
-$headers= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
-$headers .= "From: Yerlan <ereke_enu@mail.ru>\r\n"; // от кого письмо !!! тут e-mail, через который происходит авторизация
-$result =  $mailSMTP->send('almasmyltykbayev@mail.ru', 'Тема письма', 'Текст письма', $headers); // отправляем письмо
-// $result =  $mailSMTP->send('Кому письмо', 'Test', 'Текст письма', 'Заголовки письма');
-if($result === true){
-    echo "Письмо успешно отправлено";
-}else{
-    echo "Письмо не отправлено. Ошибка: " . $result;
-}
-*/
-
-$feed_url = "http://kazakh-tv.kz/rss-ru.xml";
-$content = file_get_contents($feed_url);
-$x = new SimpleXmlElement($content);
-
-$categories = array();
-$i=0;
-
-foreach($x->item as $entry) {
-     $cat = $entry->category."";
-     if (!in_array($cat, $categories)){
-         $categories[$i]=$cat;
-         echo "cat=".$cat."<br/>";
-         $i++;
-     }
+function sendEmail(){
+    require_once "SendMailSmtpClass.php"; // подключаем класс
+    $mailSMTP = new SendMailSmtpClass('ereke_enu@mail.ru', '***', 'ssl://smtp.mail.ru', 'Yerlan', 465); 
+    $headers= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n"; 
+    $headers .= "From: Yerlan <ereke_enu@mail.ru>\r\n"; 
+    $result =  $mailSMTP->send('almasmyltykbayev@mail.ru', 'Тема письма', 'Текст письма', $headers); 
+    //$result =  $mailSMTP->send('Кому письмо', 'Test', 'Текст письма', 'Заголовки письма');
+    if($result === true){
+        echo "Письмо успешно отправлено";
+    }else{
+        echo "Письмо не отправлено. Ошибка: " . $result;
+    }
 }
 
+function getCategories(){
+    $feed_url = "http://kazakh-tv.kz/rss-ru.xml";
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXmlElement($content);
+    $categories = array();
+    $i=0;
+    foreach($x->item as $entry) {
+        $cat = $entry->category."";
+        if (!in_array($cat, $categories)){
+            $categories[$i]=$cat;
+            $i++;
+        }
+    }
+}
 
 
 ?>
