fb6e8b957aef39b6264e300d2948b038898f5d5a
justme777 <ereke_enu@mail.ru>
2016-07-04 16:15:00 +0600

    Create Widget

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?
 class DataManager{
+    public $servername="localhost";
+    public $database="db_rssmailer";
+    public $username="root";
+    public $password="";
 
     function sendEmail(){
         require_once "SendMailSmtpClass.php"; 
@@ -56,6 +60,27 @@ class DataManager{
 		));
         $StatusUpdate = $statement->errorCode();
         echo "code=".$StatusUpdate;      
+    }
+
+    function getPDOStatement($query){
+        $servername="localhost";
+        $database="db_rssmailer";
+        $username="root";
+        $password="";
+        $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+        return $pdo->prepare($query);
+    }
+
+    function createWidget($rss, $userId){
+        $statement = $this->getPDOStatement("INSERT INTO widgets(rss, userId) VALUES(:rss, :userId);");
+        $statement->execute(array(
+            ':rss' => $rss,
+            ':userId'=>$userId,
+        ));
+
+        if($statement->errorCode()!="00000"){
+            var_dump($statement->errorInfo());
+        }
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 ﻿<?php
+/*
+$json = '{
+    "title": "JavaScript: The Definitive Guide",
+    "author": "David Flanagan",
+    "edition": 6
+}';
+$book = json_decode($json);
+// access title of $book object
+echo $book->title;*/
 
-echo "index page";
-
+//echo "index page";
+/*include("functions.php");
+$mng = new DataManager();
+$mng->createWidget("test rss", 1);
+*/
 ?>
