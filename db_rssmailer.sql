c2e941a5f1fcd022fdfe40405c37716dadf5679f
yerlan <ereke_enu@mail.ru>
2016-07-07 18:00:49 +0600

    db dump

--- db_rssmailer.sql
+++ db_rssmailer.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.11
+-- MySQL dump 10.10
 --
 -- Host: localhost    Database: db_rssmailer
 -- ------------------------------------------------------
--- Server version	5.5.25
+-- Server version	5.0.67-community-nt
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES cp1251 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,55 +16,74 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user`
+-- Table structure for table `subscriptions`
 --
-Use 'db_rssmailer'
 
-DROP TABLE IF EXISTS `user`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `user` (
-  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `subscriptions`;
+CREATE TABLE `subscriptions` (
+  `id` int(5) NOT NULL auto_increment,
+  `guid` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `create_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subscriptions`
+--
+
+
+/*!40000 ALTER TABLE `subscriptions` DISABLE KEYS */;
+LOCK TABLES `subscriptions` WRITE;
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `subscriptions` ENABLE KEYS */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(5) unsigned NOT NULL auto_increment,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `reg_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+LOCK TABLES `users` WRITE;
 UNLOCK TABLES;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 --
 -- Table structure for table `widgets`
 --
 
 DROP TABLE IF EXISTS `widgets`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `widgets` (
-  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(5) unsigned NOT NULL auto_increment,
   `rss` varchar(100) NOT NULL,
   `userId` int(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
+  `guid` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `widgets`
 --
 
-LOCK TABLES `widgets` WRITE;
+
 /*!40000 ALTER TABLE `widgets` DISABLE KEYS */;
-INSERT INTO `widgets` VALUES (4,'123',23),(5,'123',23),(6,'123',23),(7,'eee',1),(8,'23232',1);
-/*!40000 ALTER TABLE `widgets` ENABLE KEYS */;
+LOCK TABLES `widgets` WRITE;
+INSERT INTO `widgets` VALUES (10,'http://kazakh-tv.kz/rss-ru.xml',1,'ad11c78d7509dac830d6989471af7441');
 UNLOCK TABLES;
+/*!40000 ALTER TABLE `widgets` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -75,4 +94,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-04 11:30:21
